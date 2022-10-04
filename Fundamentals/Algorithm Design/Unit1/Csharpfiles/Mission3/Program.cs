1a68de48a48e26446d894e666ff435d474982b1a
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2022-10-04 18:44:08 -0400

    Mission 3 completed

---
+++ Fundamentals/Algorithm Design/Unit1/Csharpfiles/Mission3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Mission3
+{
+    internal class Program
+    {
+        static string OrdinalNumber(int number)
+        {
+            var lastDigit = number % 10;
+            var secondLast = 0;
+            string numEnd = "th";
+            if (number > 10)
+            {
+                secondLast = (number/10) % 10;
+            }
+            if (secondLast != 1)
+            {
+                switch (lastDigit)
+                {
+                    case 1:
+                        numEnd = "st";
+                        break;
+                    case 2:
+                        numEnd = "nd";
+                        break;
+                    case 3:
+                        numEnd = "rd";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return number.ToString() + numEnd;
+        }
+        static void Main(string[] args)
+        {
+            int[] testNum = {1, 2, 3, 10, 11, 12, 13, 21, 101, 111, 121};
+            for(int i = 0; i < testNum.Length; i++)
+            {
+                Console.WriteLine(OrdinalNumber(testNum[i]));
+            }
+        }
+    }
+}
