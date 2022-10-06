53597f0f20d45fa0c0093085b1a916d4e505a025
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2022-10-05 20:16:22 -0400

    mission 4

---
+++ Fundamentals/Algorithm Design/Unit1/Csharpfiles/Mission4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Mission4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            var streams = new List<int>();
+            var symbols = @"!@#$%^&*()_+-=[];',.\/~{}:|<>?";
+
+            for (var i = 0; i < 10; i++)
+            {
+                streams.Add(random.Next(0, 80));
+            }
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            while (true)
+            {
+                for (int x = 0; x < 80; x++)
+                {
+                    Console.Write(streams.Contains(x) ? symbols[random.Next(symbols.Length)] : ' ');
+                }
+
+                Console.WriteLine();
+                Thread.Sleep(100);
+
+                if (random.Next(3) == 0)
+                {
+                    streams.RemoveAt(random.Next(streams.Count));
+                }
+
+                if (random.Next(3) == 0)
+                {
+                    streams.Add(random.Next(0, 80));
+                }
+            }
+        }
+    }
+}
