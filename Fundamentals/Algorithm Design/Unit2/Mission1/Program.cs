585280d8f73db40f72b7d279569fbdb458318139
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2022-10-07 15:43:34 -0400

    Unit2 mission1

---
+++ Fundamentals/Algorithm Design/Unit2/Mission1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Mission1
+{
+    internal class Program
+    {
+
+        static void ShuffleList(List<string> items)
+        {
+            Random rnd = new Random();
+            for (int i = items.Count - 1; i >= 1; i--)
+            {
+                int j = rnd.Next(0, i + 1);
+                (items[i], items[j]) = (items[j], items[i]);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            var names = new List<string>() { "Allie", "Ben", "Claire", "Dan", "Eleanor" };
+            Console.WriteLine("Signed - up participants: " + String.Join(", ", names));
+            Console.WriteLine("Generating starting order ...");
+            ShuffleList(names);
+            Console.WriteLine("Starting order: " + String.Join(", ", names));
+        }
+    }
+}
