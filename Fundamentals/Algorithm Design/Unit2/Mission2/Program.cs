f9658d3c83ceb41449c5a9d8a5301b701c1eefb2
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2023-08-02 19:55:10 -0400

    complete code for mision 2 unit 2

--- Fundamentals/Algorithm Design/Unit2/Mission2/Program.cs
+++ Fundamentals/Algorithm Design/Unit2/Mission2/Program.cs
@@ -4,9 +4,23 @@ namespace Mission2
 {
     internal class Program
     {
+        static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n*Factorial(n-1);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine("{0}! = {1}", i, Factorial(i));
+            }
         }
     }
 }
